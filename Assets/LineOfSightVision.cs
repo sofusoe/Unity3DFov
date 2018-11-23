22511807b7d914484e0c92c42cf01cd57139d594
Sofus Øvretveit <sofusoe@gmail.com>
2018-11-23 01:37:16 +0100

    Mesh created from rays

--- Assets/LineOfSightVision.cs
+++ Assets/LineOfSightVision.cs
@@ -17,10 +17,12 @@ public class LineOfSightVision : MonoBehaviour
     private MeshFilter LOSMeshFilter;
 
     public Material material;
+
+    private Mesh meshDraw;
     
     void Start() {        
         LOSMeshFilter = GetComponent<MeshFilter>();
-        GetComponent<MeshRenderer>().material = material ? material : new Material(Shader.Find("Standard"));        
+        GetComponent<MeshRenderer>().sharedMaterial = material != null ? material : new Material(Shader.Find("Standard"));        
     }
 
     void Update() {
@@ -38,9 +40,9 @@ public class LineOfSightVision : MonoBehaviour
 
             if (DrawDebugLines) {
             for (int i = 0; i < pointLines.Length; i++) {
-                Debug.DrawLine(transform.position, pointLines[i][0], Color.red, 0);
+                Debug.DrawLine(transform.position, pointLines[i][0] + transform.position, Color.red, 0);
                 for(int j = 0; j < pointLines[i].Count - 1; j++) {
-                    Debug.DrawLine(pointLines[i][j], pointLines[i][j + 1], Color.red, 0);
+                    Debug.DrawLine(pointLines[i][j] + transform.position, pointLines[i][j + 1] + transform.position, Color.red, 0);
                 }                
             }
         }
@@ -56,7 +58,7 @@ public class LineOfSightVision : MonoBehaviour
         while(travelledDist < maxDist && Vector3.Angle(direction, new Vector3(direction.x, 0, direction.z)) < MaxAngle) {
             RaycastHit hit;
             if(Physics.Raycast(origin, direction, out hit, maxDist-travelledDist)) { // If a surface was hit
-                pointLine.Add(hit.point);
+                pointLine.Add(hit.point - transform.position);
                 travelledDist += hit.distance;
                 origin = hit.point;
                 Vector3 newDir = DoubleCross(direction, hit.normal);    
@@ -66,7 +68,7 @@ public class LineOfSightVision : MonoBehaviour
                     direction = newDir;
                 }
             } else {
-                pointLine.Add(origin + (direction.normalized) * (maxDist - travelledDist));
+                pointLine.Add(origin + (direction.normalized) * (maxDist - travelledDist) - transform.position);
                 break;
             }
         }
@@ -114,6 +116,61 @@ public class LineOfSightVision : MonoBehaviour
         Mesh mesh = new Mesh();
         mesh.name = "LOSMesh";
 
+        int pointCount = 1;
+        foreach(List<Vector3> pointLine in pointLines) {
+            pointCount += pointLine.Count;
+        }
+
+        Vector3[] vertices = new Vector3[pointCount];
+        int vertCount = 1;
+        for(int i = 0; i < pointLines.Length; i++) {
+            for(int j = 0; j < pointLines[i].Count; j++) {
+                vertices[vertCount] = pointLines[i][j] + new Vector3(0, 0.01f, 0); // Add padding to avoid z-fighting
+                vertCount++;
+            }
+        }
+
+        int[] triangles = new int[9000];
+
+        int triCount = 0;
+        int vertOffset = 1; // Offset from center
+        // For every point line
+        for(int i = 0; i < pointLines.Length - 1; i++) {
+            int thisIndex = 0; // Start at center for current line
+            int nextIndex = 1; // Start at 1st point for next line
+
+            int firstVertInNextLineIndex = vertOffset + pointLines[i].Count;
+            int lastVertIntThisLineIndex = vertOffset + pointLines[i].Count - 1;
+
+            for(thisIndex = 0; thisIndex < pointLines[i].Count; thisIndex++) {
+
+                triangles[triCount] = thisIndex == 0 ? 0 : thisIndex + vertOffset - 1;
+                triangles[triCount + 1] = firstVertInNextLineIndex;
+                triangles[triCount + 2] = thisIndex + vertOffset;
+                //Debug.Log(thisIndex);
+                //Debug.Log(triangles[triCount] + ": " + vertices[triangles[triCount]]);
+                //Debug.Log(triangles[triCount + 1] + ": " + vertices[triangles[triCount + 1]]);
+                //Debug.Log(triangles[triCount + 2] + ": " + vertices[triangles[triCount + 2]]);
+                triCount += 3;
+            }
+            for(nextIndex = 0; nextIndex < pointLines[i + 1].Count - 1; nextIndex++) {
+                triangles[triCount] = pointLines[i].Count + vertOffset + nextIndex;
+                triangles[triCount + 1] = pointLines[i].Count + vertOffset + nextIndex + 1;
+                triangles[triCount + 2] = lastVertIntThisLineIndex;
+                triCount += 3;
+            }
+
+            vertOffset += pointLines[i].Count; // Offset by points in this pointLine
+        }
+
+        meshDraw = mesh;
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+        mesh.RecalculateTangents();
+
         return mesh;
     }
 
@@ -142,5 +199,12 @@ public class LineOfSightVision : MonoBehaviour
         m.RecalculateNormals();
 
         return m;
+    }
+
+    void OnDrawGizmos() {
+        for(int i = 0; i < meshDraw.vertices.Length; i++) {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawSphere(meshDraw.vertices[i] + transform.position, 0.05f);
+        }
     }
 }
